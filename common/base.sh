77e03f9e40f3ceccedfdbcb3570bc212c74e53af
Soprano ◆gLOatUU2DY <soplab@soprano-asm.net>
2014-07-03 14:13:52 +0900

    add makeUserAgent

--- common/base.sh
+++ common/base.sh
@@ -6,7 +6,7 @@ if [ "$REC_DATE" == "" ]; then
 fi
 FILENAME=$output"_"$REC_DATE
 
-USER_AGENT="`$COMMON_PATH/getParam common mozilla_agent` (`$COMMON_PATH/getParam common user_agent`) radiko.jp/`$COMMON_PATH/getParam common player_ver`"
+USER_AGENT="`$COMMON_PATH/makeUserAgent JCBA \`$COMMON_PATH/getParam common version\``"
 HTTP_TIMEOUT=`$COMMON_PATH/getParam common http_timeout`
 
 TTY=`tty`
